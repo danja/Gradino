851819d53b360dd74eb66deb1f4281021b1d5a8a
Danny Ayers <danny.ayers@gmail.com>
2012-02-19 13:26:06 +0100

    slideshow experiment

---
+++ www/stuff/pp/js/pp.js
@@ -0,0 +1,30 @@
+
+// using Diaz workaround though most browser now have built-in method
+// http://www.dustindiaz.com/top-ten-javascript/)
+function getElementsByClassName(node,classname) {
+  if (node.getElementsByClassName) { // use native implementation if available
+    return node.getElementsByClassName(classname);
+  } else {
+    return (function getElementsByClass(searchClass,node) {
+        if ( node == null )
+          node = document;
+        var classElements = [],
+            els = node.getElementsByTagName("*"),
+            elsLen = els.length,
+            pattern = new RegExp("(^|\\s)"+searchClass+"(\\s|$)"), i, j;
+
+        for (i = 0, j = 0; i < elsLen; i++) {
+          if ( pattern.test(els[i].className) ) {
+              classElements[j] = els[i];
+              j++;
+          }
+        }
+        return classElements;
+    })(classname, node);
+  }
+}
+
+function doFocus() {
+	var focusElements = getElementsByClassName(document, "focus");
+	focusElements[0].focus();
+}
